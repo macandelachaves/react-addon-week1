3914f44d819a20dd91a06e4fd63e8dff694dd4d3
macandelachaves <149009525+macandelachaves@users.noreply.github.com>
2024-11-24 19:04:47 +0000

    set up dictionary project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./Candela.jpeg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo mt-3" alt="logo" />
+        <h1>Hello World!</h1>
+        <p>I'm Candela</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
